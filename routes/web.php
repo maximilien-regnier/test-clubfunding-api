a7916b555f95d5f21cd4d0ea7741e4d97a028895
Maximilien <maximilien@lumora.agency>
2025-07-07 10:32:50 +0200

    initiate  Laravel project

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is intentionally left minimal as this is an API-only application.
+| All routes should be defined in routes/api.php
+|
+*/
